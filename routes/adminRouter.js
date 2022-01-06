60c36dd7cfe175336b2d5cd8569bf39b362ddfae
Marcus Vinicius <marcus.editor77@gmail.com>
2022-01-06 18:46:26 -0300

    first commit

---
+++ routes/adminRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../controllers/authController')
+
+router.get('/', auth, (req, res) => {
+
+    if(req.user.admin){
+        res.send('Esse dado só deve ser visto por admin'
+    )} else {
+        res.status(401).send('Not Admin: Access Denied');
+    }
+})
+
+router.get('/free', auth, (req, res) => {
+    res.send('Esse dado só deve ser visto por quem está logado')
+})
+
+module.exports = router;
